808008330b9c359527d56be8b5b1227bad19ae68
Mohan <104434167+kundalaMohan@users.noreply.github.com>
2023-12-26 15:30:18 +0530

    Update s3-notification-triggers.sh

--- aws-event-triggering/s3-notification-triggers.sh
+++ aws-event-triggering/s3-notification-triggers.sh
@@ -9,7 +9,7 @@ aws_account_id=$(aws sts get-caller-identity --query 'Account' --output text)
 echo "AWS Account ID: $aws_account_id"
 
 # Set AWS region and bucket name
-aws_region="us-east-1"
+aws_region="ap-south-1"
 bucket_name="abhishek-ultimate-bucket"
 lambda_func_name="s3-lambda-function"
 role_name="s3-lambda-sns"
